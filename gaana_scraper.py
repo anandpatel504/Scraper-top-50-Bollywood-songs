440f7ee9561be5aea26b7e7b962e26f114049ce7
anandpatel504 <anand@navgurukul.org>
2022-01-18 21:35:39 +0530

    error fixed

--- gaana_scraper.py
+++ gaana_scraper.py
@@ -5,25 +5,26 @@ import webbrowser
 
 link="https://gaana.com/playlist/gaana-dj-bollywood-top-50-1"
 req = requests.get(link)
-print(req)
+# print(req)
 
 res=req.text
-print(res)
+# print(res)
 
 
 soup=BeautifulSoup(res,"html.parser")
-url = soup.findAll("div",class_="playlist_thumb_det")
+url = soup.findAll("ul",class_="list_data")
+# print(url, "url")
 url_list=[]
 gaana_naam=[]
 num=0
 for i in url:
-    url_1= i.find("a")["href"]
+    url_1= "https://gaana.com" + i.find("a")["href"]
     url_list.append(url_1)
     name=i.find("a").text
     num+=1
     print("     " ,num,name)
 # print(url_list)
-user = int(input("Enter a numner for Ganan aand bajana "))
+user = int(input("Enter a number for Gaana and bajana "))
 song = url_list[user-1]
 # print(song)
 webbrowser.open_new_tab(song)
